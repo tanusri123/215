c88d7f40d762bf9f7aecb1e78359038e4e600822
tanusri123 <67907113+tanusri123@users.noreply.github.com>
2024-06-08 19:49:49 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+const{v4:uuidv4}=require("uuid");
+app.get("/",(req,res)=>{
+    res.redirect(`/${uuidv4()}`)
+})
+app.get("/:room",(req,res)=>{
+    res.render("index",{roomId:req.params.room})
+})
+server.listen(3030);
